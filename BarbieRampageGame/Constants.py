197815c6cc367d3e10096cda8ca0c25894f944bc
Ferfoui <kenji.oku@outlook.fr>
2023-12-16 13:52:16 +0100

    Création du squelette du jeu

--- BarbieRampageGame/Constants.py
+++ BarbieRampageGame/Constants.py
@@ -1,4 +1,7 @@
-###### Les constantes sont définies dans ce fichier
+###### Les constantes sont définies dans ce fichier ######
+
+# Le nom du jeu
+GAME_NAME = "Barbie Rampage"
 
 # La taille de l'écran
 SCREEN_WIDTH = 1000
@@ -7,4 +10,4 @@ SCREEN_HEIGHT = 800
 # Les images et les sons utilisés
 ASSETS_ROOT = "ressources/assets/"
 TEXTURES_ROOT = ASSETS_ROOT + "textures/" # Le chemin des textures
-SOUNDS_ROOT = ASSETS_ROOT + "sounds/" # Le chemin des sons
+SOUNDS_ROOT = ASSETS_ROOT + "sounds/" # Le chemin des effets sonores
--- BarbieRampageGame/Main.py
+++ BarbieRampageGame/Main.py
@@ -8,6 +8,7 @@ pygame.init
 
 # Définition de la taille de l'écran
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption(GAME_NAME)
 
 run = True
 # Boucle qui va permettre de faire tourner le jeu
@@ -15,11 +16,11 @@ while run:
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            # Quitter la boucle while si l'utilisateur quitte le jeu
+            # Faire quitter la boucle si l'utilisateur quitte le jeu
             run = False
 
-    # Mise à jour de l'écran à chaques tours de boucle
+    # Mise à jour de l'écran à chaque tours de boucle
     pygame.display.update()
 
-# Fermer le programme
+# Fermeture le programme
 pygame.quit()
