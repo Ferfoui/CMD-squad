f5c5505cb64efb39547b5b3cc853ebb9b5fecac9
Ferfoui <kenji.oku@outlook.fr>
2024-02-11 14:54:16 +0100

    Ajout d'instructions pour les armes

--- BarbieRampageGame/weapon/bullets.py
+++ BarbieRampageGame/weapon/bullets.py
@@ -4,7 +4,16 @@ from constants import *
 
 # Classe pour les balles
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, scale, x: int, y: int, direction: int):
+    def __init__(self, scale, x: int, y: int, direction: int, bullet_group: pygame.sprite.Group):
+        """Crée une nouvelle balle
+
+        Args:
+            scale (int or float): nombre par lequel la taille de la texture va être multiplié
+            x (int): position sur l'axe horizontal
+            y (int): postion sur l'axe vertical
+            direction (int): direction dans laquelle la balle va, 1 si c'est vers la droite et -1 si c'est vers la gauche
+            bullet_group (pygame.sprite.Group): groupe dans lequel la balle va être ajouté
+        """
         super().__init__()
         
         self.direction = direction
@@ -33,6 +42,10 @@ class Bullet(pygame.sprite.Sprite):
         self.width = self.image.get_width()
         self.height = self.image.get_height()
         
+        # Ajoute la balle dans le groupe donné
+        # Ici pour plus d'info sur les groupes : https://www.pygame.org/docs/ref/sprite.html#pygame.sprite.Group
+        bullet_group.add(self)
+        
         
     def load_animation(self, animation_types: list[str], texture_location: str, scale) -> dict[str, list[pygame.Surface]]:
         """Charge l'animation de la balle
@@ -40,12 +53,13 @@ class Bullet(pygame.sprite.Sprite):
         Args:
             animation_types (list[str]): noms des animations
             texture_location (str): position des textures
-            scale (int or float): échelle de la taille de la texture
+            scale (int or float): nombre par lequel la taille de la texture va être multiplié
 
         Returns:
             dict[str, list[pygame.Surface]]: dictionnaire de listes de frames
         """
         animation_dict = {}
+        
         for animation in animation_types:
             animation_dict[animation] = []
 			# Compte le nombre d'images qu'il y a dans le dossier
@@ -58,8 +72,29 @@ class Bullet(pygame.sprite.Sprite):
                 animation_dict[animation].append(img)
         return animation_dict
     
+    def move(self):
+        """Fais bouger la balle
+        """
+        #TODO: Faire bouger la balle
+    
+    def check_collides(self, player: pygame.sprite.Sprite, enemy_group: pygame.sprite.Group):
+        """Vérifie les collisions entre la balle et le joueur ou un ennemi
+
+        Args:
+            player (pygame.sprite.Sprite): joueur
+            enemy_group (pygame.sprite.Group): groupe des ennemis
+        """
+        #TODO: Vérifier les collisions
+        # https://www.pygame.org/docs/ref/sprite.html#pygame.sprite.spritecollide
+
+    def update(self):
+        """Met à jour la balle
+        """
+        self.update_animation
     
     def update_animation(self):
+        """Met à jour l'animation de la balle en fonction du temps
+        """
         ANIMATION_COOLDOWN = 50
         # Met à jour l'image en fonction de la frame actuelle
         self.image = self.animation_dict[self.action][self.frame_index]
--- BarbieRampageGame/weapon/weapons.py
+++ BarbieRampageGame/weapon/weapons.py
@@ -1,29 +1,67 @@
 #Création des différentes armes du jeu
 
 import pygame
-from utils import Assets
 
 from constants import *
 import utils
 
 # La classe qui crée les armes
 class Weapon():
-    def __init__(self, weapon_name, texture_path, assets: utils.Assets, weapon_width, x, y):
+    def __init__(self, weapon_name: str, texture_path: str, assets: utils.Assets, weapon_width: int, x: int, y: int):
+        """Créé une nouvelle arme
+
+        Args:
+            weapon_name (str): nom de l'arme
+            texture_path (str): position de la texture
+            assets (utils.Assets): classe des assets
+            weapon_width (int): largeur de l'arme
+            x (int): position sur l'axe horizontal
+            y (int): position sur l'axe vertical
+        """
         self.is_grab = False
         self.flip = False
-        self.weapon_texture = self.init_textures(weapon_name, texture_path, assets, weapon_width)
+        self.weapon_texture = self.init_texture(weapon_name, texture_path, assets, weapon_width)
         self.rect = self.weapon_texture.get_rect()
-        self.rect.center = (x,y)
+        self.rect.center = (x, y)
 
-    def draw(self,screen: pygame.Surface):
+    def draw(self, screen: pygame.Surface):
+        """Affiche l'arme sur l'écran
+
+        Args:
+            screen (pygame.Surface): écran
+        """
         screen.blit(self.weapon_texture,self.rect)    
     
-    def init_textures(self, name, texture_path, assets: utils.Assets, weapon_width: int) -> pygame.Surface:
-        return assets.get_image(name, texture_path, weapon_width, 0)
+    def init_texture(self, name: str, texture_path: str, assets: utils.Assets, weapon_width: int) -> pygame.Surface:
+        """Initialise la texture de l'arme
+
+        Args:
+            name (str): nom de l'arme
+            texture_path (str): position de la texture
+            assets (utils.Assets): classe des assets
+            weapon_width (int): largeur de l'arme
+
+        Returns:
+            pygame.Surface: image de l'arme
+        """
+        return assets.get_image(name, texture_path, weapon_width)
     
     def shoot(self, direction: int):
-        pass
+        """Tire une munition
 
-class ARB4RB13(Weapon):
-    def __init__(self, assets: Assets, weapon_width, x, y):
+        Args:
+            direction (int): direction dans laquelle la balle va, 1 si c'est vers la droite et -1 si c'est vers la gauche
+        """
+        #TODO: créer une munition qui va apparaître sur le canon de l'arme
+
+class Arb4rb13(Weapon):
+    def __init__(self, assets: utils.Assets, weapon_width: int, x: int, y: int):
+        """Crée une nouvelle arme de type AR-BARB13
+
+        Args:
+            assets (utils.Assets): classe des assets
+            weapon_width (int): largeur de l'arme
+            x (int): position de l'axe horizontal
+            y (int): position de l'axe vertical
+        """
         super().__init__("AR-B4RB13", TEXTURES_ROOT + "weapons/AR_B4RB13.png", assets, weapon_width, x, y)
