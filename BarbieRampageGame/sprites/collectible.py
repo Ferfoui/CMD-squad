c021df67225607fb782463a4b79c888f9abb9d16
Kaenzo <zouine.kenzo@gmail.com>
2024-03-26 14:47:37 +0100

    class item box

--- BarbieRampageGame/sprites/collectible.py
+++ BarbieRampageGame/sprites/collectible.py
@@ -1,8 +1,12 @@
 import pygame
 
 class Collectible(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x, y):
         super().__init__()
+        self.x = x
+        self.y = y
     
 
-
+class ItemBox(Collectible):
+    def __init__(self, x, y):
+        super().__init__(x ,y)
