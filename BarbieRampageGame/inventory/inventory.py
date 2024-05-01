ad4e0f35eea79d45e57f8b749697a036474f0e02
Ferfoui <kenji.oku@outlook.fr>
2024-05-01 21:44:17 +0200

    Ajout d'une classe pour l'inventaire

---
+++ BarbieRampageGame/inventory/inventory.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Inventory:
+    def __init__(self):
+        """Crée un inventaire du joueur
+        """
+        
+        self.items = []
+        self.selected_item = None
+        self.selected_index = 0
+    
+    def add_item(self, item):
+        """Ajoute un item à l'inventaire
+        
+        Args:
+            item (Item): item à ajouter
+        """
+        self.items.append(item)
