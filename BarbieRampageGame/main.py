19ae85898a9f21fedcdac9a6bab17d172b21ac5c
Ferfoui <kenji.oku@outlook.fr>
2024-05-05 19:33:54 +0200

    Ajout d'un argument pour l'inventaire pour le spawn du player

--- BarbieRampageGame/main.py
+++ BarbieRampageGame/main.py
@@ -54,9 +54,12 @@ def timer_minute(time_milisec: int) -> str:
     hour = min // 60
     return f"{hour:02}:{min - hour * 60:02}:{sec - min * 60:02}"
 
-def spawn_player():
+def spawn_player(inventory: inventory.Inventory = None):
     """Fais réapparaître le joueur et réinitialise le monde
 
+    Args:
+        inventory (Inventory, optional): inventaire du joueur. None par défaut
+    
     Returns:
         Player: joueur recréé
     """
