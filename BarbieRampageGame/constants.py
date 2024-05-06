1f30efa6d8f54fe01520bef0644a7a7bd1cff6e3
Ferfoui <kenji.oku@outlook.fr>
2024-05-06 19:57:25 +0200

    Ajout d'un didactitiel (partiellement finit)

--- BarbieRampageGame/constants.py
+++ BarbieRampageGame/constants.py
@@ -45,7 +45,7 @@ TILE_TYPES = TILE_TYPES_WITHOUT_PLAYER_AND_ENEMIES + PLAYER_AND_ENEMIES_TILE_TYP
 # Les constantes in-game
 GRAVITY = 0.75
 
-WORLD_LIST = ["level0", "level1"]
+WORLD_LIST = ["level0", "level1", "level4"]
 
 #### Les chemins vers les fichiers ####
 
